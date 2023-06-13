06ac273cbe976f5880bf7ab3dcf72e5a926422df
gabriel <gabrielvalentesjc@hotmail.com>
2023-06-13 14:08:32 -0300

    versao incial

---
+++ index.js
@@ -0,0 +1,9 @@
+var ilustracao = window.document.getElementById("ilustracao")
+
+function TrocarImagem () {
+    ilustracao.src = "./img/cozinha.svg"
+}
+
+function ReverterImagem () {
+    ilustracao.src = "./img/loft.svg"
+}
